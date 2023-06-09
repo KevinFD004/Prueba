06f870e556acddfc64054e6e9fdbcb6e85c05e54
Kevin <kevindelgado016@gmail.com>
2023-06-09 19:32:38 -0400

    Se agrego la funcionalidad de agregar valores por consola

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,7 +4,9 @@ class Program
     static void Main(string[] args)
     {
         Suma sum = new Suma((n1,n2) => n1 + n2);
-        Console.WriteLine(sum(12,43));
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine(sum(num1,num2));
     }
 
     delegate int Suma(int num1, int num2);
